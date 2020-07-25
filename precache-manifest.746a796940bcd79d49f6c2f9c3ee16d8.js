9cac662b8a35d8bc02d8ea662a8d451bb99e511c
André Molinari <molinari.andref@gmail.com>
2020-07-25 20:36:16 -0300

    Primeiro Commit

---
+++ precache-manifest.746a796940bcd79d49f6c2f9c3ee16d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd938c5a854b3ad9a2dc9625e9b5b962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0133a65e33ea05cdf889",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "1ae418f0a150fda26723",
+    "url": "/static/css/main.464df1a8.chunk.css"
+  },
+  {
+    "revision": "0133a65e33ea05cdf889",
+    "url": "/static/js/2.34946aae.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.34946aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae418f0a150fda26723",
+    "url": "/static/js/main.147cf779.chunk.js"
+  },
+  {
+    "revision": "67e85333bb14618f3a58",
+    "url": "/static/js/runtime-main.d0c8e979.js"
+  }
+]);
